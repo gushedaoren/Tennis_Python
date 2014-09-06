143af78753771de7b09b1b4f8635cda9769b4002
oliver <gushedaoren@gmail.com>
2014-09-06 20:00:26 +0800

    Alias /static/admin /usr/lib/python2.7/site-packages/django/contrib/admin/static/admin/
        <Directory /usr/lib/python2.7/site-packages/django/contrib/admin/static/admin/>
            Order deny,allow
            Allow from all
        </Directory>

--- Tennis_Python/settings.py
+++ Tennis_Python/settings.py
@@ -86,4 +86,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static/')
 STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
