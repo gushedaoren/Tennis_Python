a6168f2a718808af5cf6b4a90b28835dda559cc6
oliver <gushedaoren@gmail.com>
2014-09-06 13:39:45 +0800

    删除django.conf

--- Tennis_Python/wsgi.py
+++ Tennis_Python/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import django.core.handlers.wsgi
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tennis_Python.settings")
 
 from django.core.wsgi import get_wsgi_application
