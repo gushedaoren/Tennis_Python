01180b78b0009ccad4b9fa9f97e83c4f3746e99d
oliver <gushedaoren@gmail.com>
2014-09-07 20:03:24 +0800

    static

--- Tennis_Python/settings.py
+++ Tennis_Python/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT =''
-STATIC_URL ='/statc/'
+STATIC_URL ='/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATICFILES_DIRS = (
 
