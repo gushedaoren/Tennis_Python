1d2d1231de40e5ee16454ac16dbca7bc6369c666
oliver <gushedaoren@gmail.com>
2014-09-07 10:06:52 +0800

    os.path.join(PROJECT_PATH, 'assets'),

--- Tennis_Python/settings.py
+++ Tennis_Python/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -94,5 +94,5 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATICFILES_DIRS = (
-    'static'
+   os.path.join(PROJECT_PATH, 'assets'),
 )
