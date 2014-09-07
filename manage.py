dd9fa8e7218c986c3a906ecaf064af4c8fbc412b
oliver <gushedaoren@gmail.com>
2014-09-07 18:43:36 +0800

    BASE_DIR+'Tennis_Python/static',

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+
+    sys.path.append('/Volumes/D/webserver/Tennis_Python/Tennis_Python')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tennis_Python.settings")
 
     from django.core.management import execute_from_command_line
