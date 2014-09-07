2f621ab26724a880c3a3ae949ec1272bbebb830b
oliver <gushedaoren@gmail.com>
2014-09-07 19:29:50 +0800

    BASE_DIR+'Tennis_Python/static',

--- Tennis_Python/settings.py
+++ Tennis_Python/settings.py
@@ -99,9 +99,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT ='/Volumes/D/webserver/Tennis_Python/static/'
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 STATIC_URL ='/statc/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATICFILES_DIRS = (
-    '/static/rest_framwork',
+
 )
