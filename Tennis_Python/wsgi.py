a2d9572b50056b7fbcf20e35a2e3f62df7d6eae5
oliver <gushedaoren@gmail.com>
2014-09-07 20:11:32 +0800

    static

--- Tennis_Python/wsgi.py
+++ Tennis_Python/wsgi.py
@@ -11,8 +11,10 @@ import os
 import sys
 import django.core.handlers.wsgi
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tennis_Python.settings")
+
 
 app_apth = "/var/www/python_sites/Tennis_Python/"
 sys.path.append(app_apth)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tennis_Python.settings")
+
 application = get_wsgi_application()
