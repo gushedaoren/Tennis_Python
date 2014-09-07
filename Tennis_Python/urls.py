4cb81c82f75c46d6b05c8d726f6a18b1ef8dd850
oliver <gushedaoren@gmail.com>
2014-09-07 11:55:10 +0800

    rest framework

--- Tennis_Python/urls.py
+++ Tennis_Python/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from django.db import router
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import routers
 from quickstart import views
