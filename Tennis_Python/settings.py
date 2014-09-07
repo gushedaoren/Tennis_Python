1154ab3d8a96a27791c85d73cc924ba4acd15d6a
oliver <gushedaoren@gmail.com>
2014-09-07 10:42:02 +0800

    STATIC_URL must end with a slash

--- Tennis_Python/settings.py
+++ Tennis_Python/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT =os.path.join(PROJECT_PATH,'static')
-STATIC_URL ='/statc'
+STATIC_URL ='/statc/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATICFILES_DIRS = (
    os.path.join(PROJECT_PATH, 'assets'),
