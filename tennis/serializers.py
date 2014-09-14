d5fcbb58ece4ee3058b8af252da0bfa56cc0f084
oliver <gushedaoren@gmail.com>
2014-09-14 12:55:25 +0800

    put snippets

---
+++ tennis/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from tennis.models import Court, BaseCity
+
+
+class BaseCitySerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = BaseCity
+        fields = ('id', 'cityName', 'cityType', 'firstLetter', 'isHot')
+
+
+class CourtSerializer(serializers.ModelSerializer):
+    city_BaseCity_Model1=BaseCitySerializer(source='city_BaseCity_Model')
+    district_BaseCity_Model1=BaseCitySerializer(source='district_BaseCity_Model')
+
+    class Meta:
+        model = Court
+        fields = ('id', 'title', 'cityType', 'address', 'phone', 'startTime','endTime','fee','courtDesc','courtCount','weight',
+        'longitude','latitude','city_BaseCity_Model1','district_BaseCity_Model1')
---
+++ tennis/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from tennis import views
+
+
+urlpatterns = patterns('',
+    url(r'^tennis/court_list$', views.CourtList.as_view()),
+    url(r'^tennis/court_detial(?P<pk>[0-9]+)/$', views.CourtDetail.as_view()),
+)
+
+urlpatterns = format_suffix_patterns(urlpatterns)
